fac157c04cdf8da3c3c6bfbd53995838285d4aea
Kelly Hodis <668@holbertonschool.com>
2019-08-18 23:17:53 -0400

    Update state

--- models/state.py
+++ models/state.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    # DBStorage class attribute
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state")
+    # FileStorage getter attribute
+    else:
+        @property
+        def cities(self):
+            """returns list of City instances with state_id matching the
+            current State id
+            """
+            objects = models.storage.all(City)
+            a_list = []
+            for obj in objects:
+                if obj.state_id == self.id:
+                    a_list.append(obj)
+            return a_list
