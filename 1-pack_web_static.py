7f1de13f8541d72b9d527bb95ff34d40614cb21a
Kelly Hodis <668@holbertonschool.com>
2019-08-20 01:48:59 -0400

    Update function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,13 +11,12 @@ def do_pack():
     '''This is a function that generates a .tgz archive from
     web_static folder.
     '''
-    try:
-        date_and_time = datetime.now()
-        date_and_time = date_and_time.strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
-        command = "tar -cvzf versions/web_static_" + date_and_time +\
-            ".tzg web_static"
-        local(command)
+    date_and_time = datetime.now()
+    date_and_time = date_and_time.strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    command = "tar -cvzf versions/web_static_" + date_and_time +\
+        ".tzg web_static"
+    if local(command).succeeded:
         return "versions/web_static_" + date_and_time + ".tzg"
-    except:
+    else:
         return None
