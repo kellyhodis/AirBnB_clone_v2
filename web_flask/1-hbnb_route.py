aa0fc084d2620e353c21380f229ea4e5302fd7e8
Kelly Hodis <668@holbertonschool.com>
2019-09-03 13:20:05 -0400

    Create 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This is a script that starts a Flask web application.
+    Listening on 0.0.0.0
+    - port 5000
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBNB():
+    ''' Function to print 'Hello HBNB!'
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    ''' Function to pring 'HBNB'
+    '''
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run()
