4d28b5ad40d21de41c454e9d1193cdd9e87c8386
Kelly Hodis <668@holbertonschool.com>
2019-09-03 14:25:48 -0400

    Create 5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" This is a script that starts a Flask web application.
+    Listening on 0.0.0.0
+    - port 5000
+"""
+
+
+from flask import abort, render_template, Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBNB():
+    ''' Function to print 'Hello HBNB!'
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    ''' Function to print 'HBNB'
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def display(text):
+    ''' Function to print 'C' followed by user input
+    '''
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def display_python(text="is cool"):
+    ''' Function to print 'Python' followed by user input
+    '''
+    return 'Python ' + text.replace("_", " ")
+
+
+@app.route('/number/<n>')
+def display_int(n=None):
+    ''' Function to print '<n> is a number'
+    '''
+    try:
+        int(n)
+        return str(n) + ' is a number'
+    except ValueError:
+        abort(404)
+
+
+@app.route('/number_template/<n>')
+def display_html(n=None):
+    ''' Function to print HTML page if n is integer
+    '''
+    try:
+        int(n)
+        return render_template('5-number.html', n=n)
+    except ValueError:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run()
