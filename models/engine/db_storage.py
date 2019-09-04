68212ecd19e0b99189141c5fcb81c5f24cd6e2a1
Kelly Hodis <668@holbertonschool.com>
2019-09-04 21:13:57 +0000

    Create close method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -89,3 +89,8 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """close a session
+        """
+        self.__session.remove()
