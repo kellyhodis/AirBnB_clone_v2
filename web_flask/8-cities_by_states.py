d9d588f4aa24fce8476bfdd30fdf50a2871bd2ca
Kelly Hodis <668@holbertonschool.com>
2019-09-05 03:41:06 +0000

    Create 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This is a script that starts a Flask web application.
+    Listening on 0.0.0.0
+    - port 5000
+"""
+
+
+from os import getenv
+from models import State
+from models import storage
+from flask import abort, render_template, Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def display_cities_by_states():
+    ''' Display a HTML page with Cities sorted by States
+    '''
+    states = storage.all("State")
+    cities = storage.all("City")
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    ''' Teardown
+    '''
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run()
